1f577f3f6ab0fd19f154810e0a50b9d4386c2c18
mariahotoiu <mariahotoiu@yahoo.com>
2017-11-16 18:17:45 +0100

    Update calculateSSR.m

--- Assignment 4/res/calculateSSR.m
+++ Assignment 4/res/calculateSSR.m
@@ -1,19 +1,38 @@
-function SSR = calculateSSR(P)
+function [SSR,error] = calculateSSR(P)
 
-initialization = ; % Should Be filled 
-relations =  ; % Should be filled
-parameters = ;% Should be filled
-delta_t = ;% Should be filled
-maxT=;% Should be filled
-Data = ; % Emperical Data
-etha = ;
+initialization =[1 1 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0] ; % Should Be filled 
+relations =[0 0.6 0.8 1 0.5 0 0 0 0 0 0 0;
+            0.7 0 0 0 0.4 0.8 0 0 0 0 0 0;
+            0.5 0 0 0 1 0 0.9 0 0 0 0 0;
+            0.6 0 0 0 0 0 0.4 0.8 0.7 0 0 0;
+            0.3 0.5 0.4 0 0 0.3 0.1 0 0 0.7 0.3 0;
+            0 0.6 0 0 0.3 0 0 0 0 0.7 0 0.2;
+            0 0 1 0.2 0.3 0 0 0.1 0 0 0.2 0;
+            0 0 0 0.5 0 0 0.4 0 0.8 0 0.5 0;
+            0 0 0 0.9 0 0 0 1 0 0 0 0;
+            0 0 0 0 0.4 0.7 0 0 0 0 0.9 0.7;
+            0 0 0 0 0.3 0 0.2 0.3 0 0.1 0 0.2;
+            0 0 0 0 0 0.9 0 0 0 0.8 0.9 0]  ; % Should be filled
+parameters =[0	0	0	0	0	0	0	0	0	0	0	0;
+0	0	0	0	0	0	0	0	0	0	0	0;
+1	1	1	1	1	1	1	1	1	1	1	1;
+2.1	1.7	2.2	2.6	3.2	2.7	2	2.2	1.5	2.3	2.8	1.1;
+0	0	0	0	0	0	0	0	0	0	0	0;
+0	0	0	0	0	0	0	0	0	0	0	0;
+0	0	0	0	0	0	0	0	0	0	0	0;
+0	0	0	0	0	0	0	0	0	0	0	0
+] ;% Should be filled
+delta_t =1 ;% Should be filled
+maxT=30;% Should be filled
+S=load('empiricalDataV1.mat');
+Data = S.Data; % Empirical Data
+etha = [0.1 0.1 0.1 0.05 0.05 0.05 0.5 0.1 0.1 0.01 0.1 0.5];
+for i=1:length(P)
+    etha(12) = P(i); % Missing parameters are equal to the input of this function
 
-etha(12) = P(1); % Missing parameters are equal to the input of this function
-
-
-
-newResutls = BDiSNModel1(initialization, relations, etha , parameters, delta_t, maxT ) ;
-
-SSR = nansum (nansum ((newResutls - Data) .^2 )) ;
-
-%%%%%%%%%%%
+    newResutls = BDiSNModel1(initialization, relations, etha , parameters, delta_t, maxT ) ;
+    %newResutls = newResutls(1:30,1:12); 
+    SSR(i) = nansum (nansum ((newResutls - Data) .^2 )) ;
+    error(i)=sqrt(SSR(i))/12;
+end
+end
