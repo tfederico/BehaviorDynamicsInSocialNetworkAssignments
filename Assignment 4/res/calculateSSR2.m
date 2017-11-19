cdda58b6b9a11b689ed9d525dda8311883424fd0
federico <federicotavella.7@gmail.com>
2017-11-19 15:07:36 +0100

    Update part 3

--- Assignment 4/res/calculateSSR2.m
+++ Assignment 4/res/calculateSSR2.m
@@ -1,6 +1,7 @@
-function [SSR] = calculateSSR2(P)
+function [error] = calculateSSR2(P)
 
 initialization =[1 1 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0] ; % Should Be filled 
+
 relations =[0 0.6 0.8 1 0.5 0 0 0 0 0 0 0;
             0.7 0 0 0 0.4 0.8 0 0 0 0 0 0;
             0.5 0 0 0 1 0 0.9 0 0 0 0 0;
@@ -13,6 +14,7 @@ relations =[0 0.6 0.8 1 0.5 0 0 0 0 0 0 0;
             0 0 0 0 0.4 0.7 0 0 0 0 0.9 0.7;
             0 0 0 0 0.3 0 0.2 0.3 0 0.1 0 0.2;
             0 0 0 0 0 0.9 0 0 0 0.8 0.9 0]  ; % Should be filled
+        
 parameters =[0	0	0	0	0	0	0	0	0	0	0	0;
 0	0	0	0	0	0	0	0	0	0	0	0;
 1	1	1	1	1	1	1	1	1	1	1	1;
@@ -20,19 +22,26 @@ parameters =[0	0	0	0	0	0	0	0	0	0	0	0;
 0	0	0	0	0	0	0	0	0	0	0	0;
 0	0	0	0	0	0	0	0	0	0	0	0;
 0	0	0	0	0	0	0	0	0	0	0	0;
-0	0	0	0	0	0	0	0	0	0	0	0
-] ;% Should be filled
+0	0	0	0	0	0	0	0	0	0	0	0] ;% Should be filled
+
 delta_t =1 ;% Should be filled
+
 maxT=30;% Should be filled
+
 S = load('empiricalDataV2.mat');
+
 Data = S.Data2; % Empirical Data
+
 %etha = [0.1 0.1 0.1 0.05 0.05 0.05 0.5 0.1 0.1 0.01 0.1 0.5];
 
 etha = P; % Missing parameters are equal to the input of this function
 
-newResutls = BDiSNModel1(initialization, relations, etha , parameters, delta_t, maxT ) ;
+newResutls = BDiSNModel1(initialization, relations, etha , parameters, delta_t, maxT );
+
 %newResutls = newResutls(1:30,1:12); 
+
 SSR = nansum (nansum ((newResutls - Data) .^2 )) ;
-%error(i)=sqrt(SSR(i)/12);
+
+error = sqrt(SSR/12);
 
 end
